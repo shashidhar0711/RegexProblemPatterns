2a11df7326a6c1c68875d1a3692f5098326e6448
shashidhar0711 <shashidhar.sasic@gmail.com>
2020-10-26 20:01:07 +0530

    [sasic] Add . Validating Phone Number Using Regular Expression

---
+++ RegexProblemPatterns/Pattern.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegexPoblemPatterns
+{
+    /// <summary>
+    /// It Consist a Pattern 
+    /// </summary>
+    class Pattern
+    {
+        public static string FIRST_NAME = "^[A-Z][a-z]{2,}$";
+        public static string LAST_NAME = "^[A-Z][a-z]{2,}$";
+        public static string EMAIL_ID = "^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*@[0-9a-zA-Z]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})*$";
+        public static string MOBILE_NUMBER = "^[+][1-9][0-9]{0,2}[-][1-9][0-9]{9}$";
+
+        /// <summary>
+        /// Validates the first name.
+        /// </summary>
+        /// <param name="fname">The fname.</param>
+        public void ValidateFirstName(string fname)
+        {
+            bool firstNameCheck = Regex.IsMatch(fname, FIRST_NAME);
+            if(firstNameCheck)
+            {
+                Console.WriteLine(fname + " :is Valid");
+            }
+            else
+            {
+                Console.WriteLine(fname + " :is Not Valid");
+            }
+        }
+
+        /// <summary>
+        /// Validates the last name.
+        /// </summary>
+        /// <param name="lname">The lname.</param>
+         public void ValidateLastName(string lname)
+        {
+            /// It Checks the Last Name and, Prints valid or not
+            bool lastNameCheck = Regex.IsMatch(lname, LAST_NAME);
+            if(lastNameCheck)
+            {
+                Console.WriteLine(lname + " :is Valid");
+            }
+            else
+            {
+                Console.WriteLine(lname + " :is Not Valid");
+            }
+
+        }
+
+        /// <summary>
+        /// Validates the email.
+        /// </summary>
+        /// <param name="email">The email.</param>
+        public void ValidateEmail(string email)
+        {
+            /// It Checks the Email and, Prints valid or not
+            bool emailCheck = Regex.IsMatch(email, EMAIL_ID);
+            if(emailCheck)
+            {
+                Console.WriteLine(email + " :is Valid");
+            }
+            else
+            {
+                Console.WriteLine(email + " :is Not Valid");
+            }
+        }
+
+        /// <summary>
+        /// Validates the phone number.
+        /// </summary>
+        /// <param name="mobileNumber">The mobile number.</param>
+        public void ValidatePhoneNo(string mobileNumber)
+        {
+            /// It Checks the First Name and, Prints valid or not
+            bool numberCheck =  Regex.IsMatch(mobileNumber, MOBILE_NUMBER);
+            if(numberCheck)
+            {
+                Console.WriteLine(mobileNumber + " :is Valid");
+            }
+            else
+            {
+                Console.WriteLine(mobileNumber + " :is Not Valid");
+            }
+        }
+    }
+}
--- RegexProblemPatterns/Program.cs
+++ RegexProblemPatterns/Program.cs
@@ -1,12 +1,56 @@
-﻿using System;
+﻿/*
+ *  C# CODING STANDARDS - LINE SPACE
+ *  NOTE - A SINGLE BLANK LINE SEPARATES THE TWO BLOCKS i.e. USING STATEMENTS, CLASS DECLARATION, etc
+ */
 
-namespace RegexProblemPatterns
+/******************************************************************************
+ *  Compilation:  C:\Users\sasic\source\repos\RegexProblem_C\bin\Debug\netcoreapp3.1\RegexProblemPatterns_C.exe
+ *  Execution:    RegexProblemPatterns_C.exe
+ *  
+ *  Purpose: Validate the User Registration By Using Regular Expression
+ *
+ *  @author  Shashidhar C
+ *  @version 1.0
+ *  @since   18-10-2020
+ *
+ ******************************************************************************/
+
+/// Including the requried assemblies in to the program
+using System;
+
+namespace RegexPoblemPatterns
 {
     class Program
     {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /// Creating a Reference for Pattern Class
+            Pattern pattern = new Pattern();
+
+            /// Taking First Name from Console to Validate
+            Console.WriteLine("Enter the First Name :");
+            string fname = Console.ReadLine();
+            pattern.ValidateFirstName(fname);
+
+            /// Taking Last Name from Console to Validate
+            Console.WriteLine("Enter the Last Name :");
+            string lname = Console.ReadLine();
+            pattern.ValidateLastName(lname);
+
+            /// Taking Email from Console to Validate
+            Console.WriteLine("Enter the Email :");
+            string email = Console.ReadLine();
+            pattern.ValidateEmail(email);
+
+            /// Taking Phone Number from Console to Validate
+            Console.WriteLine("Enter the Phone Number :");
+            string mobileNumber = Console.ReadLine();
+            pattern.ValidatePhoneNo(mobileNumber);
+
         }
     }
 }
