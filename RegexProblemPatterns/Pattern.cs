a596d7deaa602cd3176ea895d5b22aac12870326
shashidhar0711 <shashidhar.sasic@gmail.com>
2020-10-26 16:48:22 +0530

    [sasic] Add. Validating the Email Using Regex

---
+++ RegexProblemPatterns/Pattern.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegexPoblemPatterns
+{
+    /// <summary>
+    /// It Consist a Pattern 
+    /// </summary>
+    class Pattern
+    {
+        public static string FIRST_NAME = "^[A-Z][a-z]{2,}$";
+        public static string LAST_NAME = "^[A-Z][a-z]{2,}$";
+        public static string EMAIL_ID = "^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*@[0-9a-zA-Z]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})*$";
+
+        /// <summary>
+        /// Validates the method.
+        /// </summary>
+        /// <param name="fname">The fname.</param>
+        /// <returns></returns>
+        public bool ValidateFirstName(string fname)
+        {
+            /// It returns a Boolean Value ie true or false
+            return Regex.IsMatch(fname, FIRST_NAME);
+        }
+
+        public bool ValidateLastName(string lname)
+        {
+            /// It returns a Boolean Value ie true or false
+            return Regex.IsMatch(lname, LAST_NAME);
+        }
+
+        public bool ValidateEmail(string email)
+        {
+            /// It returns a Boolean Value ie true or false
+            return Regex.IsMatch(email, EMAIL_ID);
+        }
+    }
+}
--- RegexProblemPatterns/Program.cs
+++ RegexProblemPatterns/Program.cs
@@ -1,12 +1,38 @@
-﻿using System;
+﻿/*
+ *  C# CODING STANDARDS - LINE SPACE
+ *  NOTE - A SINGLE BLANK LINE SEPARATES THE TWO BLOCKS i.e. USING STATEMENTS, CLASS DECLARATION, etc
+ */
 
-namespace RegexProblemPatterns
+/******************************************************************************
+ *  Compilation:  C:\Users\sasic\source\repos\RegexProblem_C\bin\Debug\netcoreapp3.1\RegexProblemPatterns_C.exe
+ *  Execution:    RegexProblemPatterns_C.exe
+ *  
+ *  Purpose: Validate the Last Name By Using Regular Expression
+ *
+ *  @author  Shashidhar C
+ *  @version 1.0
+ *  @since   18-10-2020
+ *
+ ******************************************************************************/
+
+/// Including the requried assemblies in to the program
+using System;
+
+namespace RegexPoblemPatterns
 {
     class Program
     {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /// Creating a Reference for Pattern Class
+            Pattern pattern = new Pattern();
+            Console.WriteLine(pattern.ValidateFirstName("Shashi"));
+            Console.WriteLine(pattern.ValidateLastName("Dhar"));
+            Console.WriteLine(pattern.ValidateEmail("Shashidhar.sasic@gmail.com"));
         }
     }
 }
