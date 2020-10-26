1a2c205f53bf840a045d99241d2a950616c433f0
shashidhar0711 <shashidhar.sasic@gmail.com>
2020-10-26 16:32:55 +0530

    [sasic] Add . Validating the Last Name Using Regex

---
+++ RegexProblemPatterns/Pattern.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegexPoblemPatterns
+{
+    /// <summary>
+    /// It Consist a Pattern 
+    /// </summary>
+    class Pattern
+    {
+        public static string FIRST_NAME = "^[A-Z][a-z]{2,}$";
+        public static string LAST_NAME = "^[A-Z][a-z]{2,}$";
+
+
+        /// <summary>
+        /// Validates the method.
+        /// </summary>
+        /// <param name="fname">The fname.</param>
+        /// <returns></returns>
+        public bool ValidateFirstName(string fname)
+        {
+            /// It returns a Boolean Value ie true or false
+            return Regex.IsMatch(fname, FIRST_NAME);
+        }
+
+        public bool ValidateLastName(string lname)
+        {
+            /// It returns a Boolean Value ie true or false
+            return Regex.IsMatch(lname, LAST_NAME);
+        }
+    }
+}
