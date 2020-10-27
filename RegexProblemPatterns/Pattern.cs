8e8e112be619758aaa8f9c6f43722a09ae3e715c
shashidhar0711 <shashidhar.sasic@gmail.com>
2020-10-27 11:12:02 +0530

    [sasic] Add . Validating User Registration By Using Regex

---
+++ RegexProblemPatterns/Pattern.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegexPoblemPatterns
+{
+    /// <summary>
+    /// It Consist a Pattern 
+    /// </summary>
+    class Pattern
+    {
+        public const string USER_NAME = "^[A-Z][a-z]{2,}[ ][A-Z][a-z]{2,}$";
+        public const string EMAIL_ID = "^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*@[0-9a-zA-Z]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})*$";
+        public const string MOBILE_NUMBER = "^[+][1-9][0-9]{0,2}[-][1-9][0-9]{9}$";
+        public const string PASSWORD = "^(.{8,}|[^0-9]*|[^!#@$%^&*()<>:@?{}]*|[^A-Z])$";
+
+        /// <summary>
+        /// Validates the first name.
+        /// </summary>
+        /// <param name="fname">The fname.</param>
+        public bool ValidateFirstName(string fullName)
+        {
+            /// It will check Full Name and, It returns ture or false
+            bool firstNameCheck = Regex.IsMatch(fullName, USER_NAME);
+            if (firstNameCheck)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Validates the email.
+        /// </summary>
+        /// <param name="email">The email.</param>
+        public bool ValidateEmail(string eMail)
+        {
+            /// It Checks the Email and, It returns ture or false
+            bool emailCheck = Regex.IsMatch(eMail, EMAIL_ID);
+            if (emailCheck)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Validates the phone number.
+        /// </summary>
+        /// <param name="mobileNumber">The mobile number.</param>
+        public bool ValidatePhoneNo(string mobileNumber)
+        {
+            /// It Checks the Mobile Number and, It returns ture or false
+            bool numberCheck = Regex.IsMatch(mobileNumber, MOBILE_NUMBER);
+            if (numberCheck)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Validates the password.
+        /// </summary>
+        /// <param name="password">The password.</param>
+        public bool ValidatePassword(string password)
+        {
+            /// It Checks the Password and, It returns ture or false
+            bool passwordCheck = Regex.IsMatch(password, PASSWORD);
+            if (passwordCheck)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
--- RegexProblemPatterns/Program.cs
+++ RegexProblemPatterns/Program.cs
@@ -1,12 +1,60 @@
-﻿using System;
+﻿/*
+ *  C# CODING STANDARDS - LINE SPACE
+ *  NOTE - A SINGLE BLANK LINE SEPARATES THE TWO BLOCKS i.e. USING STATEMENTS, CLASS DECLARATION, etc
+ */
 
-namespace RegexProblemPatterns
+/******************************************************************************
+ *  Compilation:  C:\Users\sasic\source\repos\RegexProblem_C\bin\Debug\netcoreapp3.1\RegexProblemPatterns_C.exe
+ *  Execution:    RegexProblemPatterns_C.exe
+ *  
+ *  Purpose: Validate the User Registration By Using Regular Expression
+ *
+ *  @author  Shashidhar C
+ *  @version 1.0
+ *  @since   18-10-2020
+ *
+ ******************************************************************************/
+
+/// Including the requried assemblies in to the program
+using System;
+
+namespace RegexPoblemPatterns
 {
     class Program
     {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /// Creating a Reference for Pattern Class
+            Pattern pattern = new Pattern();
+
+            /// Taking First Name from Console to Validate
+            Console.WriteLine("Enter the Full Name :");
+            string fullName = Console.ReadLine();
+            Console.WriteLine(pattern.ValidateFirstName(fullName));
+
+            /// Taking Last Name from Console to Validate
+            //Console.WriteLine("Enter the Last Name :");
+            //string lastName = Console.ReadLine();
+            //Console.WriteLine(pattern.ValidateLastName(lastName));
+
+            /// Taking Email from Console to Validate
+            Console.WriteLine("Enter the Email :");
+            string eMail = Console.ReadLine();
+            Console.WriteLine(pattern.ValidateEmail(eMail));
+
+            /// Taking Phone Number from Console to Validate
+            Console.WriteLine("Enter the Phone Number :");
+            string mobileNumber = Console.ReadLine();
+            Console.WriteLine(pattern.ValidatePhoneNo(mobileNumber));
+
+            /// Taking Password from Console to Validate
+            Console.WriteLine("Enter the Password :");
+            string password = Console.ReadLine();
+            Console.WriteLine(pattern.ValidatePassword(password));
         }
     }
 }
