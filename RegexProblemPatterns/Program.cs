6176d3224622138fb6b461a594ea972866bf4ff0
shashidhar0711 <shashidhar.sasic@gmail.com>
2020-11-03 20:39:58 +0530

    [sasic] Add . Validating User Registration Using MS Test With Customized Exception

--- RegexProblemPatterns/Program.cs
+++ RegexProblemPatterns/Program.cs
@@ -11,6 +11,28 @@ namespace RegexProblemPatterns
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to User Registration");
+            /// Creating a Reference for Pattern Class
+            UserRegistration userRegistration = new UserRegistration();
+
+            /// Taking Full Name from Console to Validate
+            Console.WriteLine("Enter the Full Name :");
+            string fullName = Console.ReadLine();
+            Console.WriteLine(userRegistration.ValidateFullName(fullName));
+
+            /// Taking Email from Console to Validate
+            Console.WriteLine("Enter the Email :");
+            string eMail = Console.ReadLine();
+            Console.WriteLine(userRegistration.ValidateEmail(eMail));
+
+            /// Taking Phone Number from Console to Validate
+            Console.WriteLine("Enter the Phone Number :");
+            string mobileNumber = Console.ReadLine();
+            Console.WriteLine(userRegistration.ValidateMobileNumber(mobileNumber));
+
+            /// Taking Password from Console to Validate
+            Console.WriteLine("Enter the Password :");
+            string password = Console.ReadLine();
+            Console.WriteLine(userRegistration.ValidatePassword(password));
         }
     }
 }
